3990da03a63e71153dc3d5b106d02749c95e885d
Maurício Costa <mauricio@dbseller.com.br>
2017-01-20 15:19:47 -0200

    Adicionado método importar arquivos para o banco.

--- classes/PostgresMigration.php
+++ classes/PostgresMigration.php
@@ -13,4 +13,25 @@ class PostgresMigration extends AbstractMigration {
     {
         return new \Classes\Table($tableName, $options, $this->getAdapter());
     }
+
+    /**
+     * Import a file to database
+     * @param string path/to/file
+     * @return integer OID
+     */
+    public function importLargeObject($path)
+    {
+        $pdo = $this->getAdapter()->getConnection();
+
+        $oid = $pdo->pgsqlLOBCreate();
+        $stream = $pdo->pgsqlLOBOpen($oid, 'w');
+        $local = fopen($path, 'rb');
+
+        stream_copy_to_stream($local, $stream);
+
+        $local = null;
+        $stream = null;
+
+        return $oid;
+    }
 }
