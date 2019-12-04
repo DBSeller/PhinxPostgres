6b73d9389b7ee102b66ff4ed2e5f0fa26884c112
Fabio Egidio <fabio.egidio@dbseller.com.br>
2019-12-04 08:42:06 -0300

    Adaptada classe para versão 0.10 do Phinx

--- classes/Table.php
+++ classes/Table.php
@@ -3,38 +3,45 @@
 namespace Classes;
 
 use Phinx\Db\Adapter\AdapterInterface;
+use Phinx\Db\Table as BaseTable;
 
-class Table extends \Phinx\Db\Table
+/**
+ * Class Table
+ * @package Classes
+ */
+class Table extends BaseTable
 {
-
     /**
      * @var string
      */
     private $tableSchema;
 
     /**
-     * {@inheritdoc}
+     * Table constructor.
+     * @param $name
+     * @param array $options
+     * @param AdapterInterface|null $adapter
      */
-    public function __construct($name, $options = array(), AdapterInterface $adapter = null)
+    public function __construct($name, $options = [], AdapterInterface $adapter = null)
     {
-        $schema = 'public';
+        $name = trim($name);
 
-        if(isset($options['schema'])) {
-            $schema = $options['schema'];
-            unset($options['schema']);
+        if (!$this->hasSchema($name)) {
+            $schema = empty($options['schema']) ? $this->getSchemaName($name, $adapter) : $options['schema'];
+
+            if (empty($schema)) {
+                $schema = 'public';
+            }
+
+            $name = "{$schema}.{$name}";
         }
-
-        $this->setTableSchema($schema);
-        $this->setTableSchemaAdapter($adapter);
 
         parent::__construct($name, $options, $adapter);
     }
 
     /**
-     * Sets the schema of table.
-     *
-     * @param string $schema Schema of Table
-     * @return Table
+     * @param string $schema
+     * @return $this
      */
     public function setTableSchema($schema = 'public')
     {
@@ -43,8 +50,6 @@ class Table extends \Phinx\Db\Table
     }
 
     /**
-     * Gets the schema of table.
-     *
      * @return string
      */
     public function getTableSchema()
@@ -53,14 +58,12 @@ class Table extends \Phinx\Db\Table
     }
 
     /**
-     * Sets the schema of table on adapter to persist on database.
-     *
      * @param AdapterInterface $adapter
-     * @return Table
+     * @return $this
      */
     public function setTableSchemaAdapter(AdapterInterface $adapter)
     {
-        $adapterOptions           = $adapter->getOptions();
+        $adapterOptions = $adapter->getOptions();
         $adapterOptions['schema'] = $this->getTableSchema();
 
         $adapter->setOptions($adapterOptions);
@@ -69,11 +72,77 @@ class Table extends \Phinx\Db\Table
     }
 
     /**
-     * {@inheritdoc}
+     *
      */
     public function save()
     {
         $this->setTableSchemaAdapter($this->getAdapter());
         parent::save();
+    }
+
+    /**
+     * @param array $columns
+     * @param array $values
+     * @return BaseTable
+     */
+    public function insert(array $columns, array $values)
+    {
+        $count = count($columns);
+        $rows = [];
+
+        foreach ($values as $value) {
+            $data = [];
+
+            for ($key = 0; $key < $count; $key++) {
+                $data[$columns[$key]] = $value[$key];
+            }
+
+            $rows[] = $data;
+        }
+
+        return parent::insert($rows);
+    }
+
+    /**
+     * @param array|string $columns
+     * @param BaseTable|string $referencedTable
+     * @param array $referencedColumns
+     * @param array $options
+     * @return BaseTable
+     */
+    public function addForeignKey($columns, $referencedTable, $referencedColumns = ['id'], $options = [])
+    {
+        $referencedTable = trim($referencedTable);
+
+        if (!$this->hasSchema($referencedTable)) {
+            $referencedTable = "{$this->getSchemaName($referencedTable)}.{$referencedTable}";
+        }
+
+        return parent::addForeignKey($columns, $referencedTable, $referencedColumns, $options);
+    }
+
+    /**
+     * @param string $table
+     * @param AdapterInterface|null $adapter
+     * @return string
+     */
+    private function getSchemaName($table, AdapterInterface $adapter = null)
+    {
+        $adapter = $adapter ?: $this->getAdapter();
+
+        $row = $adapter->fetchRow(
+            "SELECT table_schema FROM information_schema.tables where table_name = '{$table}'"
+        );
+
+        return $row['table_schema'];
+    }
+
+    /**
+     * @param string $table
+     * @return bool
+     */
+    private function hasSchema($table)
+    {
+        return strpos($table, '.') !== false;
     }
 }
